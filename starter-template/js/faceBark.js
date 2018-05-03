2c7028ddf3d1a94b8cb8e29bf18d998bd533cb48
Max Faneuff <maxfaneuff@gmail.com>
2018-05-03 19:41:53 -0400

    finishing git issues

--- starter-template/js/faceBark.js
+++
@@ -1,9 +0,0 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//   var elems = document.querySelectorAll(".modal");
-//   var instances = M.Modal.init(elems, options);
-// });
-
-$(document).ready(function() {
-  $(".modal").modal();
-  $("select").formSelect();
-});
