c4a7d33219971e26800462edf8cad572e1cf9ef9
Rich <rchrdholme@gmail.com>
2018-05-05 14:09:10 -0400

    parks_db

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,25 @@
 var path = require("path");
 
+var db = require("../models");
+
 module.exports = function(app) {
   //Getting the park data used by Google Maps App
   app.get("/api/parks", function(req, res) {});
   //Getting all dogs
   app.get("/api/dogs", function(req, res) {});
+
+  app.post("/api/parkSearch", function(req, res) {
+  	console.log('called');
+  	console.log(req.body.data);
+
+  	db.Parks.findOne({
+      where: {
+        dogPark: 'yes'
+      },
+    }).then(function(dbParks) {
+      res.json(dbParks);
+      console.log(dbParks.dataValues);
+    });
+
+  })
 };
