cb3d13e652c1bf50cc08f40f33200f8ef7fd41a6
Max Faneuff <maxfaneuff@gmail.com>
2018-05-05 14:04:24 -0400

    adding .post stuff to faceBark.js, apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,4 +5,21 @@ module.exports = function(app) {
   app.get("/api/parks", function(req, res) {});
   //Getting all dogs
   app.get("/api/dogs", function(req, res) {});
+  //Posting new profiles through Dogs.js model
+  app.post("/api/new", function(req, res) {
+    console.log("New Profile:  ");
+    console.log(req.body);
+    Dog.create({
+      name: req.body.dogName,
+      breed: req.body.dogBreed,
+      picture: req.body.dogPic,
+      gender: req.body.gender,
+      owner: req.body.owner,
+      comment: req.body.dogComment,
+      email: req.body.email,
+      password: req.body.password,
+      likes_dogs: req.body.otherDogs,
+      likes_people: req.body.kids
+    });
+  });
 };
