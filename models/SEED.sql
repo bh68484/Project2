c64d35d309917a4759d62aa188ce1b049ec49511
James Keith <jamiek531@gmail.com>
2018-05-05 22:19:12 -0400

    Connected some API to database

---
+++ models/SEED.sql
@@ -0,0 +1,2 @@
+INSERT INTO Dogs (name, breed, picture, gender, owner, comment, email, password, weight, color, personality, likes_dogs, likes_people, favorite_games)
+VALUES ("Ellie", "Dachshund", "None", false, "Jamie", "Very Nice", "jmeikker", "kasjdfkjasdf", 110, "Black", "Nice", true, true, "Sleep");
