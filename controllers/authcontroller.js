55ddd1fd65b23693c3642a660fc285bd39a9e851
Brandon Harrington <bh68484@gmail.com>
2018-05-10 18:28:31 -0400

    Fixed handlebars views error.

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -2,16 +2,14 @@ var path = require("path");
 var exports = (module.exports = {});
 
 exports.signup = function(req, res) {
-  res.redirect("/");
+  // res.redirect("/");
+  res.render("dashboard");
 };
 
 exports.signin = function(req, res) {
   res.redirect("/");
 };
 
-// exports.dashboard = function(req, res) {
-//   res.sendFile(path.join(__dirname, "../public/home.html"));
-// };
 exports.dashboard = function(req, res) {
   res.render("dashboard");
 };
