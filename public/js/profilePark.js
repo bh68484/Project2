aae16de209566ad3f599a95f03f9d99ba007a053
Max Faneuff <maxfaneuff@gmail.com>
2018-05-12 10:04:06 -0400

    profilePark page will update

--- public/js/profilePark.js
+++ public/js/profilePark.js
@@ -6,8 +6,23 @@ $(document).ready(function(username) {
   console.log("clicked");
   $("#title").text("You're Going to " + currentPark + "!");
   // console.log(userid);
-  $.get("/api/getUsersDogs").then(function(dbDogs) {
-    console.log(dbDogs);
+  $.get("/api/parks").then(function(dbParks) {
+    console.log(dbParks);
+    for (var i = 0; i < dbParks.length; i++) {
+      if (currentPark === dbParks[i].name) {
+        console.log(dbParks[i].name);
+        console.log("this one");
+        $("#parkInfo").empty();
+        var newLine = $("<h4>");
+        newLine.append("Dogpark:  " + dbParks[i].dogpark + "</p>");
+        newLine.append("Field:  " + dbParks[i].field + "</p>");
+        newLine.append(
+          "Greenway Access:  " + dbParks[i].greenwayAcess + "</p>"
+        );
+        newLine.append("Restrooms:  " + dbParks[i].restrooms + "</p>");
+
+        $("#parkInfo").append(newLine);
+      }
+    }
   });
 });
-console.log("done");
